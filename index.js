89fe795120da69ae54cd325452a950d0dde80dd7
josh <joshmarcelino@protonmail.com>
2021-11-19 21:55:49 +0700

    set 1 random dot to a booby trap

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // let's make a game
 
+const random_number = (min,max) => ~~( Math.random() * (max - min)) + min
+
 const number_of_dots = 6;
 const main = document.getElementById('main')
 
 function Roulette () {
 
     let dots = [0, 0,  0, 0, 0,0];
+    dots[random_number(0, number_of_dots)] = 1;
+    console.log(dots)
 
     this.init = () => {
         for (let i = 0; i < number_of_dots; i++) {
@@ -19,8 +23,10 @@ function Roulette () {
     this.flip = (position) => {
         const dot = document.getElementById(`dot-${position}`)
 
-        if (dot) {
+        if (dots[position]) {
             dot.style.backgroundColor = '#ee3322';
+            alert('BOOM!');
+            return
         }
     }
 }
