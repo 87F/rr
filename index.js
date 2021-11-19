127bb3474bdd25565098e29bd5e78d8609634990
josh <joshmarcelino@protonmail.com>
2021-11-19 21:41:51 +0700

    generate some dots for the game

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // let's make a game
+
+const number_of_dots = 6;
+const main = document.getElementById('main')
+
+function Roulette () {
+    this.init = () => {
+        for (let i = 0; i < number_of_dots; i++) {
+            const div = document.createElement('div')
+            main.append(div)
+        }
+    }
+}
+
+(new Roulette()).init();
