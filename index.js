40f15b2ad8938fb398708a1ac486a39ffefd379c
josh <joshmarcelino@protonmail.com>
2021-11-19 21:52:30 +0700

    make dot red when clicked

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ const number_of_dots = 6;
 const main = document.getElementById('main')
 
 function Roulette () {
+
+    let dots = [0, 0,  0, 0, 0,0];
+
     this.init = () => {
         for (let i = 0; i < number_of_dots; i++) {
             const div = document.createElement('div')
+            div.id = `dot-${i}`
+            div.dataset.position = i;
             main.append(div)
+        }
+    }
+
+    this.flip = (position) => {
+        const dot = document.getElementById(`dot-${position}`)
+
+        if (dot) {
+            dot.style.backgroundColor = '#ee3322';
         }
     }
 }
@@ -16,7 +29,10 @@ const game = new Roulette();
 
 document.onclick = (e) => {
     const element = e.target;
-    if (element.tagName === 'DIV') console.log('you clicked a dot')
+    if (element.tagName === 'DIV') {
+        console.log(`you clicked dot - ${element.id}`)
+        game.flip(+element.dataset.position)
+    }
 }
 
 game.init();
